28517933d4da903835b02e34333f9da85ec46a08
Dominod <dev@dominod.fr>
2023-02-04 12:45:20 +0100

    [feat] Add a main menu

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,17 @@ mod components;
 mod systems;
 
 use bevy::{
-    prelude::{default, App, PluginGroup},
+    prelude::{default, App, PluginGroup, SystemSet},
     window::{PresentMode, WindowDescriptor, WindowPlugin},
     DefaultPlugins,
 };
 use bevy_editor_pls::EditorPlugin;
 use components::{Aim, Alive, Decay, HitCount, Move, Weapon};
 use systems::{
-    bullet_spawner, decay, despawn_health, despawn_ttl, enemy_direction_update, firing_bullet_emit,
-    key_input_update, manage_mob_spawner_timer, mob_spawner, mouse_button_input_update,
-    player_aim_update, setup, transform_update, MobSpawnEvent, SpawnBulletEvent,
+    bullet_spawner, clean_in_game, clean_main_menu, decay, despawn_health, despawn_ttl,
+    enemy_direction_update, firing_bullet_emit, key_input_update, manage_mob_spawner_timer,
+    mob_spawner, mouse_button_input_update, player_aim_update, setup_in_game, setup_main_menu,
+    start_button, transform_update, AppState, MobSpawnEvent, SpawnBulletEvent,
 };
 
 pub fn run(width: f32, height: f32) {
@@ -28,19 +29,27 @@ pub fn run(width: f32, height: f32) {
     }))
     .add_event::<SpawnBulletEvent>()
     .add_event::<MobSpawnEvent>()
-    .add_startup_system(setup)
-    .add_system(player_aim_update)
-    .add_system(mouse_button_input_update)
-    .add_system(key_input_update)
-    .add_system(transform_update)
-    .add_system(firing_bullet_emit)
-    .add_system(bullet_spawner)
-    .add_system(manage_mob_spawner_timer)
-    .add_system(mob_spawner)
-    .add_system(enemy_direction_update)
-    .add_system(despawn_health)
-    .add_system(despawn_ttl)
-    .add_system(decay);
+    .add_state(AppState::MainMenu)
+    .add_system_set(SystemSet::on_enter(AppState::MainMenu).with_system(setup_main_menu))
+    .add_system_set(SystemSet::on_update(AppState::MainMenu).with_system(start_button))
+    .add_system_set(SystemSet::on_exit(AppState::MainMenu).with_system(clean_main_menu))
+    .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(setup_in_game))
+    .add_system_set(SystemSet::on_exit(AppState::InGame).with_system(clean_in_game))
+    .add_system_set(
+        SystemSet::on_update(AppState::InGame)
+            .with_system(player_aim_update)
+            .with_system(mouse_button_input_update)
+            .with_system(key_input_update)
+            .with_system(transform_update)
+            .with_system(firing_bullet_emit)
+            .with_system(bullet_spawner)
+            .with_system(manage_mob_spawner_timer)
+            .with_system(enemy_direction_update)
+            .with_system(mob_spawner)
+            .with_system(despawn_health)
+            .with_system(despawn_ttl)
+            .with_system(decay),
+    );
 
     app.register_type::<Alive>();
     app.register_type::<Move>();
