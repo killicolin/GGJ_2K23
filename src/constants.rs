aecf6211b5ae01244717ebe461d494b70d2120bb
Killian Colin <killian.colin@virtual-it.fr>
2023-02-05 13:18:08 +0100

    [gd] constant improve, barrely fun

--- src/constants.rs
+++ src/constants.rs
@@ -5,7 +5,7 @@ pub const LORE_PARENT_CHOICE: &str = "The robots of the future failed to kill yo
 pub const LORE_INTRO: &str = "It is a beautiful day when suddenly an army of robots from the future appears !\nIt must probably be one of your clumsy future chidlren who mess up the future !!\nProtect human race from robots domination !";
 // Player starting statscargo
 pub const PLAYER_DIRECTION: Vec2 = Vec2 { x: 1.0, y: 1.0 };
-pub const PLAYER_SPEED: f32 = 100.0;
+pub const PLAYER_SPEED: f32 = 400.0;
 pub const PLAYER_DAMAGE: f32 = 0.3;
 pub const PLAYER_HEALTH: f32 = 1.0;
 pub const PLAYER_POSITION: Vec3 = Vec3::new(0.0, 0.0, 5.0);
@@ -13,7 +13,7 @@ pub const PLAYER_SCALE: Vec3 = Vec3::new(1.0, 1.0, 1.0);
 pub const PLAYER_AIM: Vec2 = Vec2 { x: 1.0, y: 1.0 };
 pub const PLAYER_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 pub const PLAYER_FIRE_RATE: f32 = 1.0;
-pub const PLAYER_BULLETS_SPEED: f32 = 500.0;
+pub const PLAYER_BULLETS_SPEED: f32 = 1000.0;
 pub const PLAYER_BULLETS_TTL: i32 = 1;
 pub const PLAYER_BULLETS: u32 = 20;
 
@@ -28,7 +28,7 @@ pub const BULLETS_DECAYS: f32 = 0.001;
 // Mob starting stats
 pub const MOB_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 pub const MOB_COLOR_HURT: Color = Color::rgb(0.8, 0.0, 0.0);
-pub const MOB_SPEED: f32 = 64.0;
+pub const MOB_SPEED: f32 = 180.0;
 pub const MOB_SPAWN_RADIUS: f32 = 700.0;
 pub const MOB_DAMAGE: f32 = 1.0;
 pub const MOB_HEALTH: f32 = 1.0;
@@ -39,7 +39,7 @@ pub const MOB_SCALE: Vec3 = Vec3::new(1.0, 1.0, 1.0);
 pub const MAX_WAVE_CALIBRATION: u32 = 9;
 pub const ENEMY_BY_LVL: [u32; 10] = [25, 50, 75, 100, 150, 200, 400, 800, 1200, 1600];
 pub const SPEED_SPAWN_BY_LVL: [f32; 10] =
-    [0.83, 0.83, 0.83, 0.83, 0.83, 0.83, 0.83, 0.83, 0.83, 0.83];
+    [0.46, 0.46, 0.46, 0.46, 0.46, 0.46, 0.46, 0.46, 0.46, 0.46];
 pub const SPAWN_TICK_BY_LVL: [u32; 10] = [1, 1, 2, 2, 2, 3, 3, 3, 3, 4];
 
 // Score parameter
